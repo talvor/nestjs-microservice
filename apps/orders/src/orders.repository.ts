044c071a3f125476b22d3b6bf43000d7a24ea205
Phillip Hall <talvor@me.com>
2023-05-29 23:03:06 +1000

    Add database module

---
+++ apps/orders/src/orders.repository.ts
@@ -0,0 +1,18 @@
+import { AbstractRepository } from "@lib/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { Order } from "./schemas/order.schema";
+import { InjectConnection, InjectModel } from "@nestjs/mongoose";
+import { Connection, Model } from "mongoose";
+
+@Injectable()
+export class OrdersRepository extends AbstractRepository<Order> {
+    protected readonly logger = new Logger(OrdersRepository.name);
+
+    constructor(
+        @InjectModel(Order.name) orderModel: Model<Order>,
+        @InjectConnection() connection: Connection
+    ) {
+        super(orderModel, connection)
+    }
+
+}
